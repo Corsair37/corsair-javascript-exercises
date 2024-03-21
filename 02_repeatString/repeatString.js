8af602c04827403a09b9c92f6971c18687c00d41
Corsair <999.ala37@gmail.com>
2024-03-21 07:59:46 -0400

    Attempt exercise 2
    This exercise requires a string to be repeated three times. Using a "for" statement allows this task to be completed by repeated the string "hey" three times under condition "i".

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
+let string = "hey";
+let num = 3;
 
+const repeatString = function(string, num){
+    for (let i = 0 ; i <= 3; i++){
+        return string
+    }
 };
 
 // Do not edit below this line
