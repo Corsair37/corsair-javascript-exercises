3dfe976615543ce5b5540f2e54b893cca8c25213
Corsair <999.ala37@gmail.com>
2024-03-21 08:32:22 -0400

    Complete exercise 3
    The solution function takes an input, converts it into a string, then splits it into individual characters and reorders the temporary array backwards. Then the characters are rejoined and that value is returned.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,7 @@
-const reverseString = function() {
+const reverseString = function(input) {
+   const strungInput = String(input);
+   const reverseInput = strungInput.split("").reverse().join("")
+   return reverseInput
 
 };
 
