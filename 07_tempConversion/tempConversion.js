a84438ab9b568b1ade2f1871ac437a15f58b7e1f
Corsair <999.ala37@gmail.com>
2024-03-22 10:15:01 -0400

    Edit function
    The function had failed the previous tests.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,12 +1,12 @@
-const convertToCelsius = function() {
+function convertToCelsius(fahrenheit) {
   const celsius = (fahrenheit - 32) * (5 / 9);
-    return Math.round(celsius * 10) / 10;
-};
+  return Math.round(celsius * 10) / 10;
+}
 
-const convertToFahrenheit = function() {
+function convertToFahrenheit(celsius) {
   const fahrenheit = (celsius * 9 / 5) + 32;
-    return Math.round(fahrenheit * 10) / 10;
-};
+  return Math.round(fahrenheit * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
