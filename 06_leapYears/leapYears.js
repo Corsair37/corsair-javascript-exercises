9daf67282bf1b3359d6feeb52f3aa273e65e4d2f
Corsair <999.ala37@gmail.com>
2024-03-22 10:04:19 -0400

    Complete exercise #6
    A simple if statement solves this exercise by testing for the remainder of the year when divided by 4, then 100 or 400.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
 const leapYears = function() {
-
+    function leapYears(year) {
+        if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    module.exports = leapYears;
+    
 };
 
 // Do not edit below this line
